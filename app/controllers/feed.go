78365acc0d1b23bb2d38324dfbf41af6d019d6dc
max <fugazister@gmail.com>
2014-03-01 18:09:46 +0700

    json marshalling

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -1,15 +1,37 @@
 package controllers
 
-import "github.com/revel/revel"
-import "net/http"
-import "io/ioutil"
+import (
+  "github.com/revel/revel"
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
+  "fmt"
+)
 
 type Feed struct {
   *revel.Controller
 }
 
+type VKArray struct {
+  Id int
+  From_id int
+  Owner_id int
+  Date int
+  Post_type string
+  Text string
+}
+
+type VKResponseBody struct {
+  Count int
+  Items []VKArray 
+}
+
+type VKResponse struct {
+  Response VKResponseBody
+}
+
 func (c Feed) Show(feedId string) revel.Result {
-  var requestUrl string = "https://api.vk.com/method/wall.get?owner_id=" + feedId
+  var requestUrl string = "https://api.vk.com/method/wall.get?count=2&v=5.12&owner_id=" + feedId
 
   resp, err := http.Get(requestUrl)
 
@@ -25,7 +47,14 @@ func (c Feed) Show(feedId string) revel.Result {
     c.Response.Status = 500
   }
 
-  result := string(body)
+  var encoded VKResponse
 
-  return c.Render(result)
+  err = json.Unmarshal(body, &encoded)
+
+  if err != nil {
+    c.Response.Status = 500
+    fmt.Println(err)
+  }
+
+  return c.Render(encoded)
 }
