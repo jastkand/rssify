e87c228ad2b199091646c79a29f45600361097e3
max <fugazister@gmail.com>
2014-03-01 15:47:46 +0700

    make vk api request and render json result

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -1,11 +1,31 @@
 package controllers
 
 import "github.com/revel/revel"
+import "net/http"
+import "io/ioutil"
 
 type Feed struct {
   *revel.Controller
 }
 
-func (c Feed) Show(feedId int) revel.Result {
-  return c.Render(feedId)
+func (c Feed) Show(feedId string) revel.Result {
+  var requestUrl string = "https://api.vk.com/method/wall.get?owner_id=" + feedId
+
+  resp, err := http.Get(requestUrl)
+
+  if err != nil {
+    c.Response.Status = 500
+  }
+
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+
+  if err != nil {
+    c.Response.Status = 500
+  }
+
+  result := string(body)
+
+  return c.Render(result)
 }
