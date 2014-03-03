28d7cdf1f1f4b61ca9e2fc03e5bdf9b2f623717b
Andrey Krivko <jastkand@gmail.com>
2014-03-04 00:33:19 +0700

    remove port from url

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(function(){
 
     if (!input.val().match(/vk.com\/(\w+)/)) return;
 
-    window.location.href = "http://rssify.me:3000/api/v1/rss?g=" + input.val();
+    window.location.href = "http://rssify.me/api/v1/rss?g=" + input.val();
 
     e.preventDefault();
   })
