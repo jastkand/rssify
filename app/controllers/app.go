7ff2fa709cf77ac6e8ecbdc171473d6ce2ad8031
Andrey Krivko <jastkand@gmail.com>
2014-03-05 00:48:16 +0700

    cleaner way to include google analytics script

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -4,6 +4,15 @@ import (
 	"github.com/revel/revel"
 )
 
+func defaultRenderArgs(c *revel.Controller) revel.Result {
+	c.RenderArgs["GoogleAnalytics"] = revel.Config.StringDefault("analytics.ga", "")
+	return nil
+}
+
+func init() {
+	revel.InterceptFunc(defaultRenderArgs, revel.BEFORE, &App{})
+}
+
 type App struct {
 	*revel.Controller
 }
--- app/init.go
+++ app/init.go
@@ -2,29 +2,9 @@ package app
 
 import (
 	"github.com/revel/revel"
-	"html/template"
-	"strings"
 )
 
 func init() {
-	revel.TemplateFuncs["gascript"] = func() template.HTML {
-		if accountId, found := revel.Config.String("analytics.account_id"); found {
-			gaScriptTemplate := `var _gaq = _gaq || [];
-_gaq.push(['_setAccount', '{{analytics_id}}']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();`
-			jsCode := strings.Replace(gaScriptTemplate, "{{analytics_id}}", accountId, -1)
-
-			return template.HTML("<script type=\"text/javascript\">" + jsCode + "</script>")
-		}
-		return template.HTML("")
-	}
-
 	// Filters is the default set of global filters.
 	revel.Filters = []revel.Filter{
 		revel.PanicFilter,             // Recover from panics and display an error page instead.
